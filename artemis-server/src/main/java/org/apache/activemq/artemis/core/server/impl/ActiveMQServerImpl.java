f1231c1893aaadff61b36b9d2f2369df2b7b7c1e
Todor Neykov <t.neykov@seeburger.com>
2024-01-09 10:37:45 +0100

    SEE - Add CallerStack to connection entry

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/ActiveMQServerImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/ActiveMQServerImpl.java
@@ -1979,6 +1979,11 @@ public class ActiveMQServerImpl implements ActiveMQServer {
    }
 
    @Override
+   public Map<String, String> getConnectionsInfo() {
+      return remotingService.getConnectionsInfo();
+   }
+
+   @Override
    public long getTotalConnectionCount() {
       return remotingService.getTotalConnectionCount();
    }

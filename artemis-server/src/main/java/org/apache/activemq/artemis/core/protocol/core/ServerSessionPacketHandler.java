45ce2bead84cb32b275bb805fca80c2e7e34dd11
Rico Neubauer <r.neubauer@seeburger.de>
2024-10-08 09:05:56 +0200

    [Story #192409] Added logging for ActiveMQIOErrorException

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/protocol/core/ServerSessionPacketHandler.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/protocol/core/ServerSessionPacketHandler.java
@@ -871,7 +871,7 @@ public class ServerSessionPacketHandler implements ChannelHandler {
                                                                      ServerSession session) {
       session.markTXFailed(e);
       if (requiresResponse) {
-         logger.debug("Sending exception to client", e);
+         ActiveMQServerLogger.LOGGER.sendingUnexpectedExceptionToClient(e);
          response = convertToExceptionPacket(packet, e);
       } else {
          ActiveMQServerLogger.LOGGER.caughtException(e);
@@ -884,7 +884,7 @@ public class ServerSessionPacketHandler implements ChannelHandler {
                                                                 boolean requiresResponse,
                                                                 Packet response) {
       if (requiresResponse) {
-         logger.debug("Sending exception to client", e);
+         ActiveMQServerLogger.LOGGER.sendingUnexpectedExceptionToClient(e);
          if (packet.isResponseAsync()) {
             response = new SessionXAResponseMessage_V2(packet.getCorrelationID(), true, e.errorCode, e.getMessage());
          } else {
@@ -901,7 +901,7 @@ public class ServerSessionPacketHandler implements ChannelHandler {
                                                                                  boolean requiresResponse,
                                                                                  Packet response) {
       if (requiresResponse) {
-         logger.debug("Sending exception to client", e);
+         ActiveMQServerLogger.LOGGER.sendingUnexpectedExceptionToClient(e);
          response = convertToExceptionPacket(packet, e);
       } else {
          ActiveMQServerLogger.LOGGER.caughtException(e);
@@ -924,7 +924,7 @@ public class ServerSessionPacketHandler implements ChannelHandler {
                                                               boolean requiresResponse,
                                                               Packet response) {
       if (requiresResponse) {
-         logger.debug("Sending exception to client", e);
+         ActiveMQServerLogger.LOGGER.sendingUnexpectedExceptionToClient(e);
          response = convertToExceptionPacket(packet, e);
       } else {
          if (e.getType() == ActiveMQExceptionType.QUEUE_EXISTS) {
